1d46f7d57f861aafd2467f70099712aedbe0d1c5
a4alaniz <a4alaniz@gmail.com>
2019-11-13 20:36:36 -0600

    comments done

--- project_backend/db/schema.rb
+++ project_backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_203443) do
+ActiveRecord::Schema.define(version: 2019_11_13_174319) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_11_11_203443) do
     t.string "abbreviation"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "comment"
   end
 
 end
